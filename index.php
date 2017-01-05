56cc434108b420ed62227142b84251ec538fa8ed
Marcus Windecker <marcus.windecker@gmail.com>
2017-01-05 10:25:23 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Coming Soon! // Marcus Windecker - Missoula Web Development</title>
+</head>
+<body>
+	<h1 style="text-align: center; margin-top: 300px;">COMING SOON!</h1>
+	
+</body>
+</html>
