0b07dd86bd0e23d3db82bbb5531e2f4426ba37c3
Marcus Windecker <marcus.windecker@gmail.com>
2017-01-11 20:04:26 -0700

    building router and beginning of mvc system

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 	require_once('controllers/Router.php'); //load routing controller
-	require_once('Routes.php'); // load route definition file
 	require_once('controllers/View.php'); // load view controller
+	require_once('Routes.php'); // load route definition file
 	
